f976b49c73bd6cba0a85f1c4796bd4f0bc6bbd65
MohamadGhanemDev <mohamad.d.ghanem@gmail.com>
2026-09-14 21:30:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// windows is an object
+// in windows we have close 
+
+/* try it in console
+    window.close();
+
+ */
+
+    window.alert("Hello from js file!");
+
+    // document => html head body..
+
+    document.write("<h1> Hello <span>World!</span> </h1>"); //not professional we use create element
+
+    // document.createElement(""); we learn it later 
+
+    console.log("Hello from js file to console");
+
